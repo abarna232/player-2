3503c22881bb0a42bd4fd885651f34421a5a5cdc
abarna232 <46838608+abarna232@users.noreply.github.com>
2019-03-25 15:54:35 +0530

    25.c

---
+++ 25.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void main()
+{
+    int n,i,a[100],m,c=0,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    m=a[0];
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<m)
+        {
+            m=a[i];
+        }
+    }
+    for(i=m;i>0;i--)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(a[j]%i==0)
+            {
+                c++;
+            }
+        }
+        if(c==n)
+        {
+            printf("%d",i);
+            break;
+        }
+    }
+}
